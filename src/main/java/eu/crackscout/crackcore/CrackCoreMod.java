48d903cab32c4a0f1816c803f44c5ba9a70a0278
getPoland <rzepka.joel@outlook.de>
2024-11-30 10:13:45 +0100

    Added HopperLogger.java

--- src/main/java/eu/crackscout/crackcore/CrackCoreMod.java
+++ src/main/java/eu/crackscout/crackcore/CrackCoreMod.java
@@ -7,21 +7,12 @@ import com.mojang.logging.LogUtils;
 
 import eu.crackscout.crackcore.events.ChestLogger;
 import eu.crackscout.crackcore.events.GeneralEventHandler;
+import eu.crackscout.crackcore.events.HopperLogger;
 import eu.crackscout.crackcore.utils.DatabaseManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,41 +22,46 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 @Mod(CrackCoreMod.MODID)
 public class CrackCoreMod {
+	
 	// Define mod id in a common place for everything to reference
     public static final String MODID = "crackcoremod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "crackcoremod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "crackcoremod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "crackcoremod" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "crackcoremod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "crackcoremod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // Creates a new food item with the id "crackcoremod:example_id", nutrition 1 and saturation 2
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // Creates a creative tab with the id "crackcoremod:example_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
-
     
+/*
+ *
+ * Adding Items and Blocks as need - not needed now (REL: ITEM-BLOCK_INT)
+ * 
+ */    
+//    // Create a Deferred Register to hold Blocks which will all be registered under the "crackcoremod" namespace
+//    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+//    // Create a Deferred Register to hold Items which will all be registered under the "crackcoremod" namespace
+//    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+//    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "crackcoremod" namespace
+//    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+//
+//    // Creates a new Block with the id "crackcoremod:example_block", combining the namespace and path
+//    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+//    // Creates a new BlockItem with the id "crackcoremod:example_block", combining the namespace and path
+//    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+//
+//    // Creates a new food item with the id "crackcoremod:example_id", nutrition 1 and saturation 2
+//    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+//            .alwaysEat().nutrition(1).saturationMod(2f).build())));
+//
+//    // Creates a creative tab with the id "crackcoremod:example_tab" for the example item, that is placed after the combat tab
+//    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+//            .withTabsBefore(CreativeModeTabs.COMBAT)
+//            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+//            .displayItems((parameters, output) -> {
+//                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+//            }).build());
+//
+//    
     private static DatabaseManager databaseManager;
     
     public CrackCoreMod() {
@@ -74,21 +70,23 @@ public class CrackCoreMod {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
+//(REL: ITEM-BLOCK_INT)     
+//        // Register the Deferred Register to the mod event bus so blocks get registered
+//        BLOCKS.register(modEventBus);
+//        // Register the Deferred Register to the mod event bus so items get registered
+//        ITEMS.register(modEventBus);
+//        // Register the Deferred Register to the mod event bus so tabs get registered
+//        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         // Register a different class as event Handler 
         MinecraftForge.EVENT_BUS.register(new GeneralEventHandler());
         MinecraftForge.EVENT_BUS.register(new ChestLogger());
+        MinecraftForge.EVENT_BUS.register(new HopperLogger());
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
+//        // Register the item to a creative tab
+//        modEventBus.addListener(this::addCreative);
 
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -107,8 +105,7 @@ public class CrackCoreMod {
 
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
 
@@ -120,12 +117,12 @@ public class CrackCoreMod {
         Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
-    }
+//    // Add the example block item to the building blocks tab
+//    private void addCreative(BuildCreativeModeTabContentsEvent event)
+//    {
+//        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+//            event.accept(EXAMPLE_BLOCK_ITEM);
+//    }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
--- src/main/java/eu/crackscout/crackcore/events/HopperLogger.java
+++ src/main/java/eu/crackscout/crackcore/events/HopperLogger.java
@@ -1,9 +1,8 @@
 /**
- * @author rzepk
+ * @author crackscout
  *
- *  Nov 29, 2024 5:03:10 AM
+ *  Nov 30, 2024 9:40:52 AM
  */
-
 package eu.crackscout.crackcore.events;
 
 import java.io.BufferedWriter;
@@ -17,121 +16,93 @@ import eu.crackscout.crackcore.CrackCoreMod;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ChestMenu;
+import net.minecraft.world.inventory.HopperMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerContainerEvent;
-//import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @SuppressWarnings("preview")
-public class ChestLogger {
-
+public class HopperLogger {
 
     private final Map<ServerPlayer, ItemStack[]> trackedInventories = new HashMap<>();
 
-    public ChestLogger() {
+    public HopperLogger() {
         // Event-Handler registrieren
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     /**
-     * Event, wenn ein Spieler mit einer Kiste interagiert.
+     * Event, wenn ein Spieler mit einem Trichter interagiert.
      */
-//	@SubscribeEvent
-//    public void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
-//        // Sicherstellen, dass die Logik nur auf der Serverseite läuft
-//        if (!event.getLevel().isClientSide) {
-//            BlockPos pos = event.getPos();
-//            var world = event.getLevel();
-//            var blockState = world.getBlockState(pos);
-//
-//            // Prüfen, ob der Block eine normale oder gefangene Kiste ist
-//            if (blockState.is(Blocks.CHEST) || blockState.is(Blocks.TRAPPED_CHEST)) {
-//                var player = event.getEntity();
-//
-//                // Nur Spieler auf dem Server protokollieren (Multiplayer)
-//                if (player instanceof ServerPlayer serverPlayer) {
-//                    logToFile(serverPlayer.getName().getString(), "Interacted with chest", pos.getX(), pos.getY(), pos.getZ());
-//                }
-//            }
-//        }
-//    }
-
-    // Check placement of: CHEST / TRAPPED CHEST, HOPPER, BARREL, SHULKER, FRUNANCE
-	@SubscribeEvent
+    @SubscribeEvent
     public void onBlockInventoryChange(BlockEvent.EntityPlaceEvent event) {
-        // Stelle sicher, dass dies serverseitig ausgeführt wird
+        // Sicherstellen, dass dies serverseitig ausgeführt wird
         if (!event.getLevel().isClientSide()) {
             var entity = event.getEntity();
             var blockState = event.getPlacedBlock();
             var pos = event.getPos();
 
             if (entity instanceof ServerPlayer serverPlayer) {
-            	// CHEST / TRAPPED CHEST
-	            if (blockState.is(Blocks.CHEST) || blockState.is(Blocks.TRAPPED_CHEST)) {
-	            	logToFile(serverPlayer.getName().getString(), "Placed a chest", pos.getX(), pos.getY(), pos.getZ());
-	            }
-	            // TODO:
-	            // HOPPER       
-	            // BAREL  
-	            // SHULKER
-	            // FURNACE 
-
-            }
-        }
-    }
-
-    
-    /**
-     * Event wenn Spieler Ksite öffnet 
-     */
-	@SubscribeEvent
-    public void onContainerOpen(PlayerContainerEvent.Open event) {
-        if (event.getEntity() instanceof ServerPlayer player) {
-            AbstractContainerMenu menu = event.getContainer();
-
-            // CHECK CHEST
-            if (menu instanceof ChestMenu chestMenu) {
-                // Kisteninventar initial speichern
-                trackedInventories.put(player, copyInventory(chestMenu));
-                logToFile(player.getName().getString(), "Opened chest", (int)getBlockPos(player).x, (int)getBlockPos(player).y, (int)getBlockPos(player).z);
-            }
-        }
-    }
-
-    /**
-     * Event, wenn ein Spieler eine Kiste schließt.
-     */
-	@SubscribeEvent
-    public void onContainerClose(PlayerContainerEvent.Close event) {
-        if (event.getEntity() instanceof ServerPlayer player) {
-            AbstractContainerMenu menu = event.getContainer();
-
-            // Nur Kisten überprüfen
-            if (menu instanceof ChestMenu chestMenu) {
-                // Vergleiche Änderungen
-                ItemStack[] previousInventory = trackedInventories.remove(player);
-                if (previousInventory != null) {
-                    logInventoryChanges(player, previousInventory, chestMenu);
+                // Prüfen, ob der Block ein Trichter ist
+                if (blockState.is(Blocks.HOPPER)) {
+                    logToFile(serverPlayer.getName().getString(), "Placed a hopper", pos.getX(), pos.getY(), pos.getZ());
                 }
             }
         }
     }
 
     /**
-     * Protokolliere Änderungen im Inventar der Kiste.
+     * Event, wenn ein Spieler einen Trichter öffnet.
      */
-    private void logInventoryChanges(ServerPlayer player, ItemStack[] oldInventory, ChestMenu chestMenu) {
-        ItemStack[] currentInventory = copyInventory(chestMenu);
+    @SubscribeEvent
+    public void onContainerOpen(PlayerContainerEvent.Open event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            AbstractContainerMenu menu = event.getContainer();
+            BlockPos hopperPos = getBlockPos(player);
+            // Prüfen, ob es sich um einen Trichter handelt
+            if (menu instanceof HopperMenu hopperMenu) {
+                // Inventar des Trichters initial speichern
+                trackedInventories.put(player, copyInventory(hopperMenu));
+                logToFile(player.getName().getString(), "Opened hopper", hopperPos.getX(), hopperPos.getY(), hopperPos.getZ());
+            }
+        }
+    }
+
+    /**
+     * Event, wenn ein Spieler einen Trichter schließt.
+     */
+    @SubscribeEvent
+    public void onContainerClose(PlayerContainerEvent.Close event) {
+        if (event.getEntity() instanceof ServerPlayer player) {
+            AbstractContainerMenu menu = event.getContainer();
+
+            // Nur Hopper überprüfen
+            if (menu instanceof HopperMenu hopperMenu) {
+                BlockPos hopperPos = getBlockPos(player);
+                // Vergleiche Änderungen
+                ItemStack[] previousInventory = trackedInventories.remove(player);
+                if (previousInventory != null) {
+                    logInventoryChanges(player, previousInventory, hopperMenu, hopperPos);
+                }
+            }
+        }
+    }
+
+    /**
+     * Protokolliere Änderungen im Inventar des Hoppers.
+     */
+    private void logInventoryChanges(ServerPlayer player, ItemStack[] oldInventory, HopperMenu hopperMenu, BlockPos hopperPos) {
+        ItemStack[] currentInventory = copyInventory(hopperMenu);
 
         for (int i = 0; i < oldInventory.length; i++) {
             ItemStack oldStack = oldInventory[i];
             ItemStack currentStack = currentInventory[i];
 
+            // Prüfe nur Änderungen im Hopper-Inventar
             if (!ItemStack.isSameItemSameTags(oldStack, currentStack)) {
                 int difference = currentStack.getCount() - oldStack.getCount();
                 String action = difference > 0 ? "Added" : "Removed";
@@ -142,52 +113,60 @@ public class ChestLogger {
                     logToFile(
                             player.getName().getString(),
                             String.format("%s %d x %s", action, count, itemName),
-                            (int)getBlockPos(player).x, (int)getBlockPos(player).y, (int)getBlockPos(player).z
+                            (int)getBlockPos(player).getX(), (int)getBlockPos(player).getY(), (int)getBlockPos(player).getZ()
                     );
                 }
             }
         }
     }
 
+
     /**
-     * Kopiert das Inventar eines ChestMenus in ein Array von ItemStacks.
+     * Kopiert das Inventar eines HopperMenus in ein Array von ItemStacks.
      */
-        
-    private ItemStack[] copyInventory(ChestMenu chestMenu) {
-        int size = chestMenu.getContainer().getContainerSize();
+    private ItemStack[] copyInventory(HopperMenu hopperMenu) {
+        int size = hopperMenu.slots.size(); // Anzahl der Slots im Menü
         ItemStack[] copy = new ItemStack[size];
         for (int i = 0; i < size; i++) {
-            copy[i] = chestMenu.getContainer().getItem(i).copy();
+            // Die Slots überprüfen und die Items kopieren
+            copy[i] = hopperMenu.slots.get(i).getItem().copy();
         }
         return copy;
     }
-    
+
 
 
     /**
      * Holt die Position des Blocks, mit dem der Spieler interagiert.
      */
-    private Vec3 getBlockPos(ServerPlayer player) {
-        BlockPos pos = player.blockPosition();
-        return new Vec3(pos.getX(), pos.getY(), pos.getZ());
+    private BlockPos getBlockPos(ServerPlayer player) {
+        return player.blockPosition();
     }
     
-    
+    @SuppressWarnings("unused")
+	private Vec3 blockPosToVec3(BlockPos pos) {
+        return new Vec3(pos.getX(), pos.getY(), pos.getZ());
+    }
+
+
+    /**
+     * Schreibt die Log-Daten in eine Datei.
+     */
     private void logToFile(String playerName, String action, int x, int y, int z) {
-    	//logToDatabase(playerName, action, x, y, z); // passing the information to the database - coming soon
+        // logToDatabase(playerName, action, x, y, z); // Unkommentieren für Datenbank-Integration
         String logEntry = String.format("[%s] Player: %s Action: %s Position: (%d, %d, %d)%n",
                 LocalDateTime.now(), playerName, action, x, y, z);
         		System.out.println(logEntry);
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("chest_log.txt", true))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("hopper_log.txt", true))) {
             writer.write(logEntry);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     @SuppressWarnings("unused")
-	private void logToDatabase(String playerName, String action, int x, int y, int z) {
-        String query = "INSERT INTO chest_logs (player_name, action, x, y, z, timestamp) VALUES (?, ?, ?, ?, ?, NOW())";
+    private void logToDatabase(String playerName, String action, int x, int y, int z) {
+        String query = "INSERT INTO hopper_logs (player_name, action, x, y, z, timestamp) VALUES (?, ?, ?, ?, ?, NOW())";
         try {
             CrackCoreMod.getDatabaseManager().executeUpdate(query, playerName, action, x, y, z);
         } catch (Exception e) {
